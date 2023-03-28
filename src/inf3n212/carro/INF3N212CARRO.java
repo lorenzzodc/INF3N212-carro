18e63fb9e9bd159fbc2ae46de4c0d1a9b138e149
Lorenzzo Duval Castilhos <631510300@senacinformatica.br>
2023-03-28 19:19:02 -0300

    first commit

---
+++ src/inf3n212/carro/INF3N212CARRO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inf3n212.carro;
+
+/**
+ *
+ * @author 631510300
+ */
+public class INF3N212CARRO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
