e9c6f463c4b3cb3af115283bd668fbc173dcf8f9
Lorenzzo Duval Castilhos <631510300@senacinformatica.br>
2023-04-03 21:50:12 -0300

    Valida CPF e metodo cadastrar pessoa

--- src/inf3n212/carro/INF3N212CARRO.java
+++ src/inf3n212/carro/INF3N212CARRO.java
@@ -9,6 +9,9 @@ import controller.CPessoa;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import javax.xml.transform.OutputKeys;
+import model.Carro;
+import model.Pessoa;
+import util.Validadores;
 
 /**
  *
@@ -19,6 +22,7 @@ public class INF3N212CARRO {
     public static final String VERMELHO = "\u001B[31m";
     public static final String RESET = "\u001B[30m";
     public static final String VERDE = "\u001B[32m";
+    public static final String AZUL = "	\u001B[36m";
 
     public static CPessoa cadPessoa = new CPessoa();
 
@@ -48,7 +52,6 @@ public class INF3N212CARRO {
                         oopSM = leiaNumInt();
                         switch (oopSM) {
                             case 1:
-                                System.out.println("Cadasto");
                                 if (opM == 1) {
                                     cadastrarPessoa();
                                 } else {
@@ -56,17 +59,16 @@ public class INF3N212CARRO {
                                 }
                                 break;
                             case 2:
-                                System.out.println("Edita");
-                                 if (opM == 1) {
+                                if (opM == 1) {
                                     editarPessoa();
-                                } else  {
+                                } else {
                                     editarCarro();
                                 }
 
                                 break;
                             case 3:
                                 System.out.println("Lista");
-                                 if (opM == 1) {
+                                if (opM == 1) {
                                     listarPessoa();
                                 } else {
                                     listarCarro();
@@ -75,7 +77,7 @@ public class INF3N212CARRO {
                                 break;
                             case 4:
                                 System.out.println("Deleta1");
-                                 if (opM == 1) {
+                                if (opM == 1) {
                                     deletarPessoa();
                                 } else {
                                     deletarCarro();
@@ -121,7 +123,7 @@ public class INF3N212CARRO {
         System.out.println("1- ger.Pessoa");
         System.out.println("2- ger. carro");
         System.out.println("0 - sair");
-        System.out.print("Digite aqui:\t");
+        System.out.print("Digite aqui:");
     }//fim menuP
 
     public static void subMenu(int opm) {
@@ -133,38 +135,96 @@ public class INF3N212CARRO {
             subM = "Carro";
         }
 
-        System.out.println("--Ger. " + subM + "---");
+        System.out.println(VERDE + "--Ger. " + subM + "---" + RESET);
         System.out.println("1 - Cadastrar" + subM);
         System.out.println("2- Editar" + subM);
         System.out.println("3- Listar" + subM + "s");
         System.out.println("4 - Deletar" + subM);
         System.out.println("0 - voltar");
-        System.out.print("Digite aqui:\t");
+        System.out.print("Digite aqui:");
     }//fim subMenu
 
     private static void cadastrarPessoa() {
-        System.out.println("Pessoa");
+        System.out.println(AZUL + "--Cadastro de Pessoa--" + RESET);
+        int idPessoa;
+        String nome;
+        String cpf;
+        String endereco;
+        String telefone;
+        boolean tcpf = true;
+        do {
+            System.out.print("Digite o CPF\t");
+            cpf = leia.nextLine();
+            tcpf = Validadores.isCPF(cpf);
+            if (tcpf) {
+                if (cadPessoa.getPessoaCPF(cpf) != null) {
+                    System.out.println(VERMELHO + "CPF já cadastrado");
+                    System.out.println("1 - Tentar novamente");
+                    System.out.println("2 - Cancelar cadastro");
+                    System.out.print("Digite aqui:\t");
+                    int op = leiaNumInt();
+                    if (op == 2) {
+                        return;
+                    }
+                } else {
+                    tcpf = false;
+                }
+            } else {
+                System.out.println(VERMELHO + "CPF inválido" + RESET);
+                System.out.println("1 - Tentar novamente");
+                System.out.println("2 - Cancelar cadastro");
+                System.out.print("Digite aqui:\t");
+                int op = leiaNumInt();
+                if (op == 2) {
+                    return;
+                }
+            }
+        } while (tcpf);
+        System.out.print("Informe o nome: ");
+        nome = leia.nextLine();
+        System.out.print("Informe o endereço: ");
+        endereco = leia.nextLine();
+        System.out.print("Informe o Telefone: ");
+        telefone = leia.nextLine();
+        idPessoa = cadPessoa.geraID();
+      Pessoa p = new Pessoa(idPessoa, nome, cpf, endereco, telefone);
+      cadPessoa.addPessoa(p);
+        System.out.println(VERDE+p.getNome()+" cadastrado com sucesso!"+RESET);
     }
 
     private static void cadastrarCarro() {
-        System.out.println("Carro1");}
+        System.out.println("Carro");
+    }
 
     private static void editarPessoa() {
+
     }
 
     private static void editarCarro() {
+
     }
 
     private static void listarPessoa() {
+        System.out.println(AZUL + "-- Lista de Pessoas" + RESET);
+        for (Pessoa pessoa : cadPessoa.getPessoa()) {
+            System.out.println(pessoa.toString());
+        }
     }
 
     private static void listarCarro() {
+        System.out.println(AZUL + "-- Lista de Carros" + RESET);
+        for (Carro carro : cadCarro.getCarros()) {
+            System.out.println(carro.toString());
+
+        }
     }
 
     private static void deletarPessoa() {
+
     }
 
     private static void deletarCarro() {
+
     }
 
 }//fim da classe
